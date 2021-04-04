847a4fc3aba407876768ace2e722b8844f13fd61
t1m4 <731ruslan00@mail.ru>
2021-04-04 20:12:54 +0300

    Add support point

--- core/forms.py
+++ core/forms.py
@@ -17,3 +17,10 @@ class ResetForm(forms.Form):
 class PasswordResetEmailForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
     double_password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
+
+class SupportForm(forms.Form):
+    name = forms.CharField(max_length=40, label='Имя')
+    fio = forms.CharField(max_length=40, label='Фамилия')
+    email = forms.EmailField(label='Почта')
+    title = forms.EmailField(label='Заголовок')
+    message = forms.CharField(widget=forms.Textarea, max_length=1024, label='Напишите сообщение')
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 # Create your views here.
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+
+from core.forms import SupportForm
+from techshop.settings import EMAIL_HOST_USER
 
 
 class IndexView(View):
@@ -13,12 +19,31 @@ class IndexView(View):
 
 
 class SupportView(View):
+    template_name = 'core/html/support.html'
+    form_class = SupportForm
+    context = {'errors': ''}
+    success_url = 'core-index'
+
     def get(self, request, *args, **kwargs):
-        pass
+        form = self.form_class()
+        self.context['form'] = form
+        self.context['errors'] = ''
+        self.context['range'] = range(3)
+        return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
-        pass
-
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            try:
+                html_message = "I need some support, please asnwer me to {}, my message  {}".format(
+                    form.cleaned_data['email'],
+                    form.cleaned_data['message']
+                )
+                send_mail("Support", html_message,
+                          EMAIL_HOST_USER, ['731ruslan00@mail.ru'])
+                return redirect(reverse(self.success_url))
+            except:
+                raise Http404
 class CategoriesView(View):
     def get(self, request, *args, **kwargs):
         pass
