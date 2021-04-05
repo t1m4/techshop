2d276a41300f3706bb7134e0a4e4fe7430091274
t1m4 <731ruslan00@mail.ru>
2021-04-05 10:10:11 +0300

    Create new template filter

---
+++ core/templatetags/tags.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+@register.filter(name='price')
+def cut(value):
+    print(value)
+    value = round(value, 2)
+    return str(value).replace('.', ',')
