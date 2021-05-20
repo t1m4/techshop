7c0c17bc4136c9c8580a0a9703815a52c3fb21a3
t1m4 <731ruslan00@mail.ru>
2021-05-20 06:40:46 +0300

    fix form in orer

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,10 @@ import datetime
 from django import forms
 from django.forms import DateInput
 
-SELECT_CHOICES = [('price', 'Цена (низкая)'), ('-price', 'Цена (высокая)'), ('-name', 'Название А-Я'), ('name', 'Название Я-А')]
+SELECT_CHOICES = [('price', 'Цена (низкая)'), ('-price', 'Цена (высокая)'), ('-name', 'Название А-Я'),
+                  ('name', 'Название Я-А')]
+
+
 class LoginForm(forms.Form):
     email = forms.EmailField(label="Почта")
     password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
@@ -14,12 +17,15 @@ class RegisterForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
     double_password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
 
+
 class ResetForm(forms.Form):
     email = forms.EmailField(label='Почта')
+
 
 class PasswordResetEmailForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
     double_password = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
+
 
 class SupportForm(forms.Form):
     name = forms.CharField(max_length=40, label='Имя')
@@ -32,6 +38,7 @@ class SupportForm(forms.Form):
 class ProductForm(forms.Form):
     amount = forms.IntegerField(min_value=1, max_value=100, label='Количество')
 
+
 class BasketForm(forms.Form):
     amount = forms.IntegerField(min_value=1, max_value=100, label='Количество')
 
@@ -39,17 +46,23 @@ class BasketForm(forms.Form):
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=255, label='Количество', required=False)
 
+
 class SortForm(forms.Form):
     choice = forms.ChoiceField(widget=forms.Select, choices=SELECT_CHOICES, label="Сортировка")
+
 
 class AccountForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='Имя')
     last_name = forms.CharField(max_length=30, label='Фамилия')
     address = forms.CharField(max_length=50, label='Адрес')
 
+
 from django.contrib.admin import widgets
+
+
 class OrderSearchForm(forms.Form):
-    # start_day = forms.DateField(initial=datetime.date.today, widget=forms.DateInput, label='Дата начала')
-    start_day = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=range(2020, 2030)), label='Дата начала')
-    end_day = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=range(2020, 2030)), label='Дата конца')
+    start_day = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=range(2020, 2030)),
+                                label='Дата начала')
+    end_day = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=range(2020, 2030)),
+                              label='Дата конца')
     category = forms.CharField(max_length=100, label='Категория', required=False)
